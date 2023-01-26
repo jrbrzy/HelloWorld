2b4f24624ba72bb581ead49bbde8fc6eddcf7b9f
Jairo Munoz <jrbrzy@icloud.com>
2023-01-26 10:45:10 -0700

    stretch 8

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var Textview: UITextView!
+    
+    
+    
+    
+    
+    @IBOutlet weak var PlacerHolder: UITextField!
+    
+    
+    @IBOutlet weak var Words: UIImageView!
+    
+   
+    
     override func viewDidLoad() {
+        
+        
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    @IBAction func Button(_ sender: Any) {
+        Textview.text = "you super qt"
+//        init(Textview: Textv.text) {
+//            self.Textview = Textview
+        
+    
+        }
 }
+    
+
 
